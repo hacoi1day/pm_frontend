ec860f2da488630751b45034d8369697e36987c2
havt <vuha98.tn@gmail.com>
2021-04-15 11:30:01 +0700

    add fullCalendar

--- src/features/CheckInOut/index.vue
+++ src/features/CheckInOut/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    Checkin master
-  </div>
+  <b-card
+      header="Lịch làm việc"
+      header-tag="header"
+    >
+    <FullCalendar :options="calendarOptions" />
+  </b-card>
 </template>
 
 <script>
+import FullCalendar from '@fullcalendar/vue';
+import viLocale from '@fullcalendar/core/locales/vi';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 export default {
-  name: 'check-in'
+  name: 'check-in',
+  components: {
+    FullCalendar,
+  },
+  data () {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin ],
+        initialView: 'dayGridMonth',
+        locales: [ viLocale ],
+        locale: 'vi'
+      }
+    }
+  }
 }
 </script>
 
